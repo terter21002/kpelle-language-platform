732139a6dff32e1ffbf0ff4fdc9e93b8bb8deb2f
terter21002 <terterthedev@gmail.com>
2025-02-07 12:42:33 +0400

    feat(frontend): react@19 starter project using tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-xl p-4 m-4">Start Coding in React 19</h1>
+      <p className="bg-gray-300 p-2 rounded-sm text-3xl">hello worlds</p>
+    </div>
+  );
+}
+
+export default App;
