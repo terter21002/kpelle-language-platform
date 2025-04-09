1754582b99dd2470b4efe127d6428040e236729a
terter21002 <terterthedev@gmail.com>
2025-04-09 13:29:26 -0500

    feat(frontend): specific language page

--- frontend/src/components/_common/Layout.component.tsx
+++ frontend/src/components/_common/Layout.component.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import DashboardHeader from "./DashboardHeader.component";
 import Footer from "./MainFooter";
+import { useLocation } from "react-router-dom";
 
 const Layout = () => {
+  const location = useLocation();
+  const footerClass =
+    location.pathname == "/language-id"
+      ? "shape-fill-yellow"
+      : "shape-fill-white";
+
   return (
     <div className="min-h-screen relative">
       <header className=" bg-gray-100 z-10">
@@ -14,7 +21,7 @@ const Layout = () => {
         <Outlet />
       </main>
 
-      <Footer />
+      <Footer className={footerClass} />
     </div>
   );
 };
